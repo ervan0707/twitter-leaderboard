f907f312c8ac94088ed776e85e6620bb67a0eb75
Ryan Choi <ryan.choi@gmail.com>
2016-01-08 14:35:05 -0800

    secure

--- app/settings_my.py
+++ app/settings_my.py
@@ -6,12 +6,13 @@ except ImportError, exp:
 DEBUG = True
 SECRET_KEY = ''
 
-SOCIAL_AUTH_TWITTER_KEY = ''
-SOCIAL_AUTH_TWITTER_SECRET = ''
+SOCIAL_AUTH_TWITTER_KEY = 'fNSSzvmyZcaq8Z5aY9Q1HqSk9'
+SOCIAL_AUTH_TWITTER_SECRET = 'cRQocZcqef1vtTMRVn4KPkCdEPZNAJf6rlbqoXAdKEwLSRHFPJ'
 
-TWITTER_ACCESS_TOKEN = ''
-TWITTER_ACCESS_TOKEN_SECRET = ''
+TWITTER_ACCESS_TOKEN = '54256387-sKGolBCJAgVnpNqHduI8x0YxCuiowgTljW0QvBo6s'
+TWITTER_ACCESS_TOKEN_SECRET = '1Dns9yQlE8i4JEiDjNqmwceJ7D1piZmYQgl6lTqNm34cY'
 
+# Uncomment for local database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
